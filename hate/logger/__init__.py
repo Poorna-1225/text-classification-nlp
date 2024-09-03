c29ce892205c44f9d9e811106778c9be65cffbfb
Poorna-1225 <nagasaikumarjampani19918@gmail.com>
2024-09-03 17:50:58 -0400

    environment created

---
+++ hate/logger/__init__.py
@@ -0,0 +1,16 @@
+import os
+import logging
+from datetime import datetime
+
+log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path =  os.path.join(os.getcwd(),'logs','log_file')
+os.makedirs(logs_path, exist_ok = True)
+
+log_file_path = os.path.join(logs_path, log_file)
+
+logging.basicConfig(
+
+                    filename =  log_file_path,
+                    format = '[%(ascitime)s] %(lineno)d %(name)s - %(message)s',
+                    level =  logging.INFO
+)
