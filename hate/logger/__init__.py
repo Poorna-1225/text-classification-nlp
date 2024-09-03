cded45aae746c1ef098f480934011a2feb1ede25
Poorna-1225 <nagasaikumarjampani19918@gmail.com>
2024-09-03 18:25:09 -0400

    logger updated

--- hate/logger/__init__.py
+++ hate/logger/__init__.py
@@ -2,15 +2,15 @@ import os
 import logging
 from datetime import datetime
 
-log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path =  os.path.join(os.getcwd(),'logs','LOG_FILE')
 os.makedirs(logs_path, exist_ok = True)
 
-log_file_path = os.path.join(logs_path, log_file)
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
 
-                    filename =  log_file_path,
-                    format = '[%(ascitime)s] %(lineno)d %(name)s - %(message)s',
+                    filename =  LOG_FILE_PATH,
+                    format = '[%(ascitime)s ] %(lineno)d %(name)s - %(levelname)s- %(message)s',
                     level =  logging.INFO
 )
