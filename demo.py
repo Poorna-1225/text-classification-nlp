b2530ae4c9af64b83f02b251b1a4af08fc5c2548
Poorna-1225 <nagasaikumarjampani19918@gmail.com>
2024-09-03 20:24:47 -0400

    cloud configuration done

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from hate.logger import logging
-logging.info("welcome to out project")
+from hate.exception import CustomException
+import sys
+
+from hate.configuration.gcloud_syncer import GCloudSync
+
+
+obj = GCloudSync()
+obj.sync_folder_from_gcloud("hate_speech-nlp","dataset.zip","data/dataset.zip")
+
 
